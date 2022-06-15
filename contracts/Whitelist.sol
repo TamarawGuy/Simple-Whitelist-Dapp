269e7b067cec95b4dc176b57b74e2d1e937bd985
Viktor <viktor.m.rumenov@gmail.com>
2022-06-15 17:23:50 +0200

    Added smart contract logic

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -1,4 +1,26 @@
-//SPDX-License-Identifier: Unlicense
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract Whitelist {}
+contract Whitelist {
+    uint256 public maxWhitelistedAddresses;
+    uint256 public numberOfWhitelistedAddresses;
+    mapping(address => bool) public whitelistedAddresses;
+
+    constructor(uint256 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+
+    function addAddressToWhitelist() public {
+        require(
+            !whitelistedAddresses[msg.sender],
+            "This address is already in whitelist"
+        );
+        require(
+            numberOfWhitelistedAddresses < maxWhitelistedAddresses,
+            "Limit of whitelisted addresses reached!"
+        );
+
+        whitelistedAddresses[msg.sender] = true;
+        numberOfWhitelistedAddresses += 1;
+    }
+}
